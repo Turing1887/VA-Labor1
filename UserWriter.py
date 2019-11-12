6ee3dd014f573706f0ffece7d57cad005ba4f7ac
Tim Zschage <timzschage@gmail.com>
2019-11-12 22:29:15 +0100

    added UserWriter
    * wip-> overwrites existing line

---
+++ UserWriter.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+usercount = 0
+user_data = {}
+# user_data['user'] = []
+pauses = []
+filename = 'data.txt'
+
+class UserWriter:
+
+  def __init__(self, name):
+    self.name = name
+
+  def readUserData(self):
+    with open(filename) as json_file:
+      if os.stat(filename).st_size != 0:
+        user_data = json.load(json_file)
+        for p in user_data['user']:
+          print('Name: ' + p['name'])
+          print('Pauses: ' + str(p['pauses']))
+          print('')
+        print(user_data)
+      else:
+        print('No User Data Yet')
+      
+      
+  def addUserData(self,pause):
+    pauses.append(pause)
+
+  def writeUserData(self):
+    user_data.update({
+      'user':[
+        {
+          'name': self.name,
+          'pauses': pauses 
+        }
+      ] 
+    })
+    print(user_data)
+    with open(filename, 'w+') as outfile:
+      json.dump(user_data, outfile)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from tkinter import *
 import audio
+import UserWriter
+
+#Set Username
+print("New User: ")
+username = str(input())
+
+#Initialize UserWriter Class
+uw = UserWriter.UserWriter(username)
+#First read of data
+uw.readUserData()
 
 def submit():
     slider_value = w.get()
     print(slider_value)
     pause = slider_value/1000-0.22
+    uw.addUserData(pause)
     audio.step+=1
-    audio.playPreset(audio.presets, audio.step, )
+    audio.playPreset(audio.presets, audio.step, pause)
 
 def play():
     slider_value = w.get()
@@ -19,23 +30,13 @@ root = Tk()
 root.geometry("800x800")
 
 
-# mainframe = Frame(width=768, height=576, bg="", colormap="new")
-# mainframe.pack()
-#logo = PhotoImage(file="../images/python_logo_small.gif")
-# w1 = Label(root, image=logo).pack(side="right")
-# explanation = """At present, only GIF and PPM/PGM
-# formats are supported, but an interface
-# exists to allow additional image file
-# formats to be added easily."""
-# w2 = Label(root,
-#            justify=LEFT,
-#            padx = 10,
-#            text=explanation).pack(side="left")
 playbutton = Button(root, text="PLAY", fg="black", command= lambda:play())
 playbutton.pack()
 w = Scale(root, from_=0, to=5000, length=500, label="Set Pause", showvalue=0, orient=HORIZONTAL)
 w.pack()
 submitbutton = Button(root, text="SUBMIT", fg="black", command= lambda:submit())
 submitbutton.pack()
+finishbutton = Button(root, text="FINISH", fg="black", command= lambda:uw.writeUserData())
+finishbutton.pack()
 
 root.mainloop()
